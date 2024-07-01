7ed45c2cc3207a93afa0990b29f411af7e65432f
ashish07-cloud <172781723+ashish07-cloud@users.noreply.github.com>
2024-07-01 10:51:50 +0000

    date

---
+++ JS-BASIC01/date.js
@@ -0,0 +1,18 @@
+// Dates
+
+// let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toJSON);
+// console.log(myDate.toLocaleDateString);
+
+// let myDate = new Date()
+// // let myCreateDate = new Date(2023, 0, 23)
+// let myCreateDate = new Date(2023, 0, 23, 5, 3)
+// console.log(myCreateDate.toDateString());
+let myCreatedDate = new Date("01-14-2023")
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
