41e571c0fa3d5d386e2302036c02f3dd2671f911
ashish07-cloud <172781723+ashish07-cloud@users.noreply.github.com>
2024-07-01 10:14:20 +0000

    math and num

---
+++ JS-BASIC01/math_and_num.js
@@ -0,0 +1,33 @@
+// const score = 400
+// console.log(score);
+
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2)); // to give fixed 0 (zeeros ) after decimal if it is more
+
+// const otherNumber = 123.8966
+// console.log(otherNumber.toPrecision(3));//=> it takes the priority to the number before the decimal
+
+//++++++++++++++++++ MATHS ++++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.7));
+// console.log(Math.round(4.3));
+
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+
+// console.log(Math.random());
+// console.log((Math.random() *10 ) +1);
+// console.log(Math.floor(Math.random()*10) +1 );
+
+// const min = 10
+// const max = 20
+
+// console.log(Math.random() *(max - min + 1 ) + min)
+
+
+
