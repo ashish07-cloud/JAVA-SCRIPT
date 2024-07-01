98ef427850c5b2aab160bf7494ec1809f7a8cdeb
ashish07-cloud <172781723+ashish07-cloud@users.noreply.github.com>
2024-07-01 05:54:32 +0000

    changingdatatypes

---
+++ JS-BASIC01/03_changingdatatypes.js
@@ -0,0 +1,18 @@
+let score = "33abc"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//"33" => 33
+//"33abc" =>NaN
+//true => 1; false => 0;
+
+let bolleanIsLoggedIn = Boolean(IsLoggedIn)
+console.log();
+
+//"" => false
+//"dsgfd" => true
