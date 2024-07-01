df5fd689eece710393c333c3c8b3bb3057396244
ashish07-cloud <172781723+ashish07-cloud@users.noreply.github.com>
2024-07-01 06:55:40 +0000

    operation

---
+++ JS-BASIC01/operations.js
@@ -0,0 +1,44 @@
+// let str1 = "ASHISH";
+// let str2 = "Rai"
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1 + "2");
+// console.log("1" +2 +2);
+// console.log(1 +2 +"2");
+
+// console.log(+true);
+
+// let gameCounter = 100
+// gameCounter++;
+// ++gameCounter;
+
+// console.log(gameCounter);
+
+/**********************COMPARISON-OPERATORS******************************/ 
+
+// console.log(3 > 4)     
+// console.log(3 < 4)
+// console.log(3 === 4)
+// console.log(3 >= 4)
+// console.log(3 <=4)
+// console.log(3 == 4)
+
+
+// console.log("2" > 1)  //true
+// console.log("02" > 1)  //true because it automatically converts it to number
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+// === - this is called strict check this check value as well as data type also 
+
+
+
+
