425a91ee5a1eab009597fe1f94debe929c713bc7
ashish07-cloud <172781723+ashish07-cloud@users.noreply.github.com>
2024-07-01 05:09:10 +0000

    datatypes

---
+++ JS-BASIC01/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict" // treat all js code as newer version
+
+//alert(4+5) //=> we are using nodejs, not browser
+
+let name ="ashish"
+let age = 23
+let isLoggedIn = false
+let state = null
+
+// DATA-TYPES
+//number => range is from 2 to power of 53
+//bigint => is used for very large numbers like trading websites
+//string =>""
+//boolean => true/false
+//null => standalone value
+// indefined =>
+//symbol => unique - to clarify any object unique
+
+console.log(typeof state);
+console.log(typeof null); // object
+console.log(typeof undefined);// undefined
