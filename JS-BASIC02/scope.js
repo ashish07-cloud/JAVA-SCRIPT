d35c698c0c3b005d449b93d9f2a5ef34557c4f3d
ashish07-cloud <172781723+ashish07-cloud@users.noreply.github.com>
2024-07-02 10:04:55 +0000

    scope

---
+++ JS-BASIC02/scope.js
@@ -0,0 +1,36 @@
+let  a= 300
+if (true) {
+    let a = 10
+    const b= 20
+    // console.log("inner:", a);
+    
+}
+
+// console.log(a);
+// for (let i = 0; i < array.length; i++) {
+//     const element = array[i];
+    
+// }
+
+function one(){
+    const username = "ashish"
+
+    function two (){
+        const website = "instagram"
+        console.log(username);
+    }
+    // console.log(website)
+
+    two()
+}
+// one()
+if(true){
+    const username = "ashish"
+    if (username === "ashish"){
+        const website = "instagram"
+        console.log(username + website);
+    }
+    // console.log(website);
+}
+// console.log(username)
+
