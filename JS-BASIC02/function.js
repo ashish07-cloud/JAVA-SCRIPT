2f4e7e25568dea95ba220aff27103c47d9da07b8
ashish07-cloud <172781723+ashish07-cloud@users.noreply.github.com>
2024-07-02 09:22:45 +0000

    function

--- JS-BASIC02/function.js
+++ JS-BASIC02/function.js
@@ -12,7 +12,7 @@ function addTwoNumbers(number1 , number2){
 }
 const result = addTwoNumbers(3, 5)
 
-console.log("Result:", result);
+// console.log("Result:", result);
 
 function loginUserMessage(username) {
     if (username === undefined) {
@@ -24,4 +24,32 @@ function loginUserMessage(username) {
 }
 
 // console.log(loginUserMessage("ashish"));
-console.log(loginUserMessage());
+// console.log(loginUserMessage());
+
+//rest and spread operator 
+//...=> variable name => rest operator 
+
+function calculateCarPrice(...num1){
+    return num1
+}
+
+// console.log(calculateCarPrice(200,300,400));
+
+const user = {
+    username: "ashish",
+    price: 200
+}
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is $
+    {anyobject.price} `);
+}
+handleObject(user)
+
+
+//function into array
+
+const myNewArray = [200, 300, 400, 500]
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+console.log(returnSecondValue(myNewArray));
